75a9edc68ca4f9dce1ce7ce1ded88c67d869ecd3
Hayder Espinoza <hayderespit@gmail.com>
2025-04-07 13:34:18 -0500

    Update orders page message to reflect beer instead of meal

--- app/(tabs)/orders/page.tsx
+++ app/(tabs)/orders/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
     <Container className="flex h-screen flex-col pb-16">
       <div className="py-6 pl-4">
         <h1 className="text-2xl font-semibold text-black">Your Orders</h1>
-        <p className="text-manatee text-md font-normal">Wait for the best meal</p>
+        <p className="text-manatee text-md font-normal">Wait for the best beer</p>
       </div>
 
       <Suspense
